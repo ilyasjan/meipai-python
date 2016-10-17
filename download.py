4d11991b362edb15284c4616cc097acc7b5ea3c1
Fish& <ilyas5137@163.com>
2016-10-17 12:48:26 +0800

    Upgrade to beta version

--- download.py
+++ download.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*
+#第三方包: bs4,py-term
 from bs4 import BeautifulSoup
 import urllib2
-import os
+import term
 
 ##  get html content to soup
 def getUrlCon(url):
-  return BeautifulSoup(urllib2.urlopen(url).read(),"html.parser")
+  return BeautifulSoup(urllib2.urlopen(str(url)).read(),"html.parser")
 
 ##  get soup
 def getUrl(soup):
@@ -13,17 +15,24 @@ def getUrl(soup):
 
 ##  get mp4 file name
 def getFileName(url):
-  return url[url.rindex('/')+1:len(url)]
+  filename=url[url.rindex('/')+1:len(url)]
+  return filename
 
 ##  download file
 def DownLoad(mediaUrl):
+  term.writeLine("--------------------------------------------------------------------------------",term.yellow)
+  term.writeLine("开始地址:"+mediaUrl,term.dim)
   url=getUrl(getUrlCon(mediaUrl))
+  term.writeLine("文件地址:"+url,term.blue)
   filename=getFileName(url)
   f=open(filename,'wb')
   f.write(urllib2.urlopen(url).read())
+  term.writeLine("已下载:"+filename,term.cyan)
+  term.writeLine("--------------------------------------------------------------------------------",term.yellow)
   f.close()
+  term.write("\n\n\n\n")
 
 ##  read file list and download
-flist=open('list.txt','r')
+flist=open('list2.txt','r')
 for line in flist.readlines():
-    DownLoad(line)
+    DownLoad(line.strip())
